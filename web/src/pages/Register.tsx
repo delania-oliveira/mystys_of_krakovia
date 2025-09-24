d55d9e0bd72fb2a311a918bc2037d51e09f8f558
gmkoeb <gmkoeb@gmail.com>
2025-09-23 22:34:51 -0300

    fix axios ip and remove axios.ts

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { data, Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';

2c1ef3f4c5f513f95d83f5e0c3881f8d0534c686
Gabriel Manika <gmkoeb@gmail.com>
2025-09-25 18:27:30 -0300

    add new columns to character table
    - Added health, mana and experience columns to character table

---
+++ server/src/db/migrations/0002_oval_johnny_storm.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "characters" ADD COLUMN "health" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "characters" ADD COLUMN "mana" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "characters" ADD COLUMN "experience" integer NOT NULL;
--- server/src/db/schema/characters.ts
+++ server/src/db/schema/characters.ts
@@ -9,5 +9,8 @@ export const characters = pgTable("characters", {
   z_position: doublePrecision().notNull().default(0),
   name: text().notNull(),
   class: integer().notNull(),
-  level: integer().notNull().default(1)
+  level: integer().notNull().default(1),
+  health: integer().notNull(),
+  mana: integer().notNull(),
+  experience: integer().notNull(),
 })

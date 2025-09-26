bcece76cd4d3ebd8b585f2d033715133835f8813
Gabriel Manika <gmkoeb@gmail.com>
2025-09-26 19:50:16 -0300

    return account id on successful login

--- server/src/http/routes/login.ts
+++ server/src/http/routes/login.ts
@@ -16,7 +16,7 @@ router.post("/login", async (req: Request<{}, {}, LoginProps>, res: Response) =>
   try {
     const existing = await db.select().from(accounts).where(eq(accounts.account_name, username))
     if (existing.length > 0 && comparePassword(password, existing[0].password)) {
-        res.status(200).json({ message: "Login successful!" });
+        res.status(200).json({ message: existing[0].id });
     } else {
         res.status(401).json({ message: "Login failed. Wrong username or password!" });
     } 

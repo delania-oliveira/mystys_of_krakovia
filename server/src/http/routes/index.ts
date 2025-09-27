ec9b6ade20ee2168e5a79558d027b4430cbefa5b
Gabriel Manika <gmkoeb@gmail.com>
2025-09-27 11:13:52 -0300

    include character creation

--- server/src/http/routes/index.ts
+++ server/src/http/routes/index.ts
@@ -3,11 +3,13 @@ import register from './register'
 import health_check from './health_check'
 import check_username from './check_username'
 import get_account_characters from './get_account_characters'
+import create_account_characters from './get_account_characters'
 
 export default [
   login,
   register,
   health_check,
   check_username,
-  get_account_characters
+  get_account_characters,
+  create_account_characters
 ]

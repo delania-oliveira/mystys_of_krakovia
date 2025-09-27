6503d0a86253efda8bba26ba37463403df011123
Gabriel Manika <gmkoeb@gmail.com>
2025-09-27 11:25:14 -0300

    fix create account character route import

--- server/src/http/routes/index.ts
+++ server/src/http/routes/index.ts
@@ -3,7 +3,7 @@ import register from './register'
 import health_check from './health_check'
 import check_username from './check_username'
 import get_account_characters from './get_account_characters'
-import create_account_characters from './get_account_characters'
+import create_account_character from './create_account_character'
 
 export default [
   login,
@@ -11,5 +11,5 @@ export default [
   health_check,
   check_username,
   get_account_characters,
-  create_account_characters
+  create_account_character
 ]

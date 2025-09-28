762d22228087def0ec859d5103b4a99c0c3bf8e7
Gabriel Manika <gmkoeb@gmail.com>
2025-09-28 15:24:42 -0300

    fix check_username route props

--- server/src/http/routes/check_username.ts
+++ server/src/http/routes/check_username.ts
@@ -5,12 +5,7 @@ import { eq } from 'drizzle-orm';
 
 const router = Router()
 
-interface LoginProps {
-  username: string
-  password: string
-}
-
-router.post("/check_username", async (req: Request<{}, {}, LoginProps>, res: Response) => {
+router.post("/check_username", async (req: Request<{}, {}, {username: string}>, res: Response) => {
   const { username } = req.body;
   try {
     const existing = await db.select().from(accounts).where(eq(accounts.account_name, username))

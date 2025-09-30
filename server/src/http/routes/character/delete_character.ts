aae0d3f5b10a04edc9527f2b9d344970869ffeb4
Gabriel Manika <gmkoeb@gmail.com>
2025-09-30 14:00:11 -0300

    change misleading response status in delete route

--- server/src/http/routes/character/delete_character.ts
+++ server/src/http/routes/character/delete_character.ts
@@ -6,17 +6,22 @@ import { authenticateToken } from '../../middleware/auth';
 
 const router = Router()
 
-router.delete("/characters/:character_name", authenticateToken, async (req: Request<{}, {}, {character_name: string}>, res: Response) => {
+router.delete("/characters/:character_name", authenticateToken, async (req: Request<{character_name: string}, {}, {}>, res: Response) => {
   const account_id = (req as any).account_id;
-  const { character_name } = req.body
+  const { character_name }  = req.params
+
   try {
     const existing = await db.select().from(schema.characters).where(eq(schema.characters.name, character_name))
-    if (existing.length == 0 || existing[0].account_id != account_id) {
-        res.status(401).json( { message: `Character with name ${character_name} doesn't exist!` });
-    } else {
-      await db.delete(schema.characters).where(eq(schema.characters.name, existing[0].name))
-      res.status(200).json({ message: "Character deleted with success!" });
+    if (existing.length == 0 ) {
+      res.status(404).json( { message: `Character with name ${character_name} not found` });
+    } 
+    
+    if (existing[0].account_id != account_id) {
+      return res.status(403).json({ message: `You don't have permission to delete '${character_name}'.` });
     }
+    
+    await db.delete(schema.characters).where(eq(schema.characters.name, character_name));
+    res.status(200).json({ message: "Character deleted with success!" });
   } catch (error) {
     console.log(error)
     res.status(401).json({ message: "Database error!" });       

c218c4da91bce292076766597ad23a8bfeacbef8
[Delania Oliveira] <[delania.oliveiraa@gmail.com]>
2025-10-03 15:37:33 -0300

    add login/logout dinamic navbar

--- web/src/components/layout/Navbar.tsx
+++ web/src/components/layout/Navbar.tsx
@@ -1,9 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../ui/button';
 import { Download } from 'lucide-react';
 import logoImg from '@/assets/MOK_LOGO4.png'
+import { useAuth } from '@/hooks/useAuth';
 
 export function Navbar() {
+  const { isAuthenticated, logout } = useAuth()
+  const navigate = useNavigate()
+
+  function handleLogout() {
+    logout()
+    navigate('/login')
+  }
+
   return (
     <nav className="fixed top-0 left-0 w-full bg-black/30 backdrop-blur-sm z-20">
       <div className="container mx-auto h-22 flex items-center justify-between px-4">
@@ -18,12 +27,18 @@ export function Navbar() {
           <Link to="/" className="hover:text-primary transition-colors">
             Início
           </Link>
-          <Link to="/sobre" className="hover:text-primary transition-colors">
+          {/* <Link to="/sobre" className="hover:text-primary transition-colors">
             Sobre
-          </Link>
-          <Link to="/login" className="hover:text-primary transition-colors">
-            Login
-          </Link>
+          </Link> */}
+          {isAuthenticated ? (
+            <button onClick={handleLogout} className="cursor-pointer hover:text-primary transition-colors">
+              Logout
+            </button>
+          ) : (
+            <Link to="/login" className="hover:text-primary transition-colors">
+              Login
+            </Link>
+          )}
           <Button asChild size="lg" className="mt-6 text-lg font-bold">
             <Link to="/download">
               <Download color='white' size={24} strokeWidth={3} />

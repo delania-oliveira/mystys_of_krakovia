d03c5234ef583554eef9bc1d7e684af08d3e13f2
Gabriel Manika <gmkoeb@gmail.com>
2025-10-05 09:13:59 -0300

    remove unused console logs

--- server/src/http/routes/account/change_password.ts
+++ server/src/http/routes/account/change_password.ts
@@ -20,9 +20,6 @@ router.post("/change_password", authenticateToken, async (req: Request<{}, {}, C
   try {
     const account = await db.select().from(schema.accounts).where(eq(account_id, schema.accounts.id))
     const match = await comparePassword(account[0].password, currentPassword);
-    console.log(match)
-    console.log(currentPassword)
-    console.log(account[0].password)
     if (!match) return res.status(400).json({message: "Current Password incorrect!"})
     const hashedPassword = await hashPassword(newPassword)
     await db.update(schema.accounts).set({password: hashedPassword}).where(eq(schema.accounts.id, account_id))

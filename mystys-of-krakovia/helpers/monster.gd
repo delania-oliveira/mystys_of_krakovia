214d8de3f8d02d1fff122cb69ffc55f14f7f550b
gmkoeb <gmkoeb@gmail.com>
2025-10-06 20:41:27 -0300

    handle target ui health updates

--- mystys-of-krakovia/helpers/monster.gd
+++ mystys-of-krakovia/helpers/monster.gd
@@ -2,11 +2,9 @@ extends Node
 
 func set_monster_stats(monster, value):
 	monster.speed = value.speed
-	monster.attack_damage = value.attack
 	monster.get_node("Name").set_text(value.name)
 	monster.position = Vector3(value.x, value.y, value.z)
 	monster.spawn_position = Vector3(value.x, value.y, value.z)
 	monster.max_health = value.health
 	monster.current_health = value.health
-	monster.detection_range = value.detectionRange
 	monster.character_name = value.name
--- mystys-of-krakovia/tests/npcs/monsters/monster.gd
+++ mystys-of-krakovia/tests/npcs/monsters/monster.gd
@@ -1,82 +1,35 @@
-extends CharacterBody3D  # or Node3D if no physics needed
-var monster_id
-@export var speed = 0.0
-@export var detection_range = 10.0
-@export var attack_range = 2.0
-@export var attack_cooldown = 2.0
-var attack_damage = 0.0
+extends Node3D
+
+var id
+var speed = 0.0
 var is_targeting = false
-var target_velocity = Vector3.ZERO
-const LERP_SPEED = 10.0
 var spawn_position = Vector3.ZERO
-var target_player: Node3D = null
-var attack_timer = 0.0
 var room
-var was_idle
-var max_health = 0.0
-var current_health = 0.0
-var character_name = ""
 var model
+var current_health = 0.0
+var max_health = 0.0
+var character_name = ""
+var target_position = Vector3.ZERO
+var LERP_SPEED = 10.0
+var network_position = Vector3.ZERO
 
-func _physics_process(delta):
-	attack_timer -= delta
-
-	# Find the closest player
-	var closest_dist = detection_range
-	target_player = null
-	for player in get_tree().get_nodes_in_group("players"):
-		if player is CharacterBody3D:
-			var dist = global_position.distance_to(player.global_position)
-			if dist < closest_dist:
-				closest_dist = dist
-				target_player = player
-	if target_player && !target_player.dead:
-		move_toward_player(delta, target_player)
-		if global_position.distance_to(target_player.global_position) <= attack_range:
-			try_attack_player()
-	else:
-		return_to_spawn_point()
-
-func try_attack_player():
-	if attack_timer <= 0.0:
-		if "take_damage" in target_player:
-			target_player.take_damage(target_player.player_key, monster_id)
-		attack_timer = attack_cooldown
-		
-func return_to_spawn_point():
-	var dist_to_spawn = global_position.distance_to(spawn_position)
-	var direction = (spawn_position - global_position).normalized()
-	if dist_to_spawn > 0.1:
-		velocity = direction * speed
-		move_and_slide()
-	else:
-		velocity = Vector3.ZERO
-		global_position = spawn_position
-	_send_monster_state(direction, null)
+func _process(delta):
+	position = position.lerp(network_position, LERP_SPEED * delta)
 	
-	
-func move_toward_player(delta, target_player):
-	var dir = (target_player.global_position - global_position).normalized()
-	var distance_to_player = global_position.distance_to(target_player.global_position)
-	var move_direction = Vector3.ZERO
-	if distance_to_player > attack_range:
-		var dir_normalized = dir.normalized()
-		move_direction = dir_normalized
-		
-	velocity = move_direction * speed
-	move_and_slide()
-	_send_monster_state(move_direction, target_player)
+func _on_attack_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("players"):
+		var player_session_id = body.id # Assuming your player node has its session_id
+		room.send("moveMonster", {
+			"monsterId": id,
+			"targetId": player_session_id,
+			"isTargeting": true
+		})
 
-func _send_monster_state(move_direction: Vector3, target_player):
-	var is_idle = move_direction == Vector3.ZERO
-	var payload = {
-			"x": move_direction.x,
-			"y": 0,
-			"z": move_direction.z,
-			"monster_id": monster_id
-	}
-	if target_player:
-		payload["targetId"] = target_player.player_key
-	if is_idle != was_idle:
-		room.send("moveMonster", payload)
-		was_idle = is_idle
+
+func _on_aggro_area_body_exited(body: Node3D) -> void:
+	if body.is_in_group("players"):
+		room.send("moveMonster", {
+			"monsterId": id,
+			"targetId": "",
+			"isTargeting": false
+		})

7ccf8312cb5600a1c6f9e1cee323efc57006f908
Gabriel Manika <gmkoeb@gmail.com>
2025-10-06 21:14:50 -0300

    fix change password route password comparison

--- server/src/http/routes/account/change_password.ts
+++ server/src/http/routes/account/change_password.ts
@@ -19,7 +19,7 @@ router.post("/change_password", authenticateToken, async (req: Request<{}, {}, C
   if (!currentPassword || !newPassword) return res.status(400).json({message: "Must provide current or new password!"})
   try {
     const account = await db.select().from(schema.accounts).where(eq(account_id, schema.accounts.id))
-    const match = await comparePassword(account[0].password, currentPassword);
+    const match = account[0].password == currentPassword
     if (!match) return res.status(400).json({message: "Current Password incorrect!"})
     const hashedPassword = await hashPassword(newPassword)
     await db.update(schema.accounts).set({password: hashedPassword}).where(eq(schema.accounts.id, account_id))

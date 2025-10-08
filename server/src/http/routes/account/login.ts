96d34b0bc71f44650bcd9f949663711ac4d02a0f
Gabriel Manika <gmkoeb@gmail.com>
2025-10-08 16:43:37 -0300

    change login route to return 404 on user not found

--- server/src/http/routes/account/login.ts
+++ server/src/http/routes/account/login.ts
@@ -17,17 +17,21 @@ router.post("/login", async (req: Request<{}, {}, LoginParams>, res: Response) =
   const { username, password } = req.body;
   try {
     const existing = await db.select().from(accounts).where(eq(accounts.account_name, username))
+    if (existing.length === 0) {
+      return res.status(404).json({ message: "Login failed. Username not found!" });
+    }
+
     const match = await comparePassword(password, existing[0].password)
-    if (existing.length > 0 && match) {
-        const token = jsonwebtoken.sign(
-          { account_id: existing[0].id },
-          env.PRIVATE_KEY,
-          { expiresIn: "30d" }
-        )
-        res.status(200).json({ username, token });
-    } else {
-        res.status(401).json({ message: "Login failed. Wrong username or password!" });
-    } 
+
+    if (!match) {
+      return res.status(401).json({ message: "Login failed. Wrong password!" });
+    }
+    const token = jsonwebtoken.sign(
+      { account_id: existing[0].id },
+      env.PRIVATE_KEY,
+      { expiresIn: "30d" }
+    )
+    res.status(200).json({ username, token });
   } catch (error) {
     console.log(error)
     res.status(401).json({ message: "Database error!" });       

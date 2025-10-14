d09fd02faa4b04b6cdfe5a1445f90fc1b13fbfdd
Gabriel Manika <gmkoeb@gmail.com>
2025-10-14 20:52:09 -0300

    add flame arrow skill to archer

--- server/src/data/skills/skills_registry.ts
+++ server/src/data/skills/skills_registry.ts
@@ -2,10 +2,13 @@ import { skills, registerSkill } from "./skills";
 import { DefaultSkillArcher, DefaultSkillMage } from "./auto_attack";
 import { ArcaneExplosionMage } from "./arcane_explosion";
 import { MultiShotArcher } from "./multi_shot";
+import { FlameArrowArcher } from "./flame_arrow";
 
 registerSkill(DefaultSkillArcher);
+registerSkill(MultiShotArcher)
+registerSkill(FlameArrowArcher)
+
 registerSkill(DefaultSkillMage);
 registerSkill(ArcaneExplosionMage)
-registerSkill(MultiShotArcher)
 
 export { skills };

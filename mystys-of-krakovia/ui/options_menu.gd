e44f62bbbc395e37a62758bc89b0ec0d0ef8a709
Delania <109484507+delania-oliveira@users.noreply.github.com>
2025-10-17 18:35:52 -0300

    Add character avatars and UI assets
    Introduced new avatar images for all classes in multiple resolutions, character selection UI assets, and Cinzel font files. Updated related Godot import files and made modifications to character creation and selection screens to support the new assets.

--- mystys-of-krakovia/ui/options_menu.gd
+++ mystys-of-krakovia/ui/options_menu.gd
@@ -2,15 +2,15 @@ extends Control
 
 const CONFIG_PATH = "user://settings.cfg"
 
-@onready var return_button = $MarginContainer/VBoxContainer/ButtonsSection/ReturnButton
-@onready var resolution_select = $MarginContainer/VBoxContainer/GraphicsSection/ResolutionSection/SelectResolution
-@onready var master_slider = $MarginContainer/VBoxContainer/VolumeSection/VBoxContainer/MasterVolumeSlider
-@onready var save_button = $MarginContainer/VBoxContainer/ButtonsSection/SaveButton
-@onready var fullscreen_check = $MarginContainer/VBoxContainer/GraphicsSection/VBoxContainer/ModeSection/Fullscreen
-@onready var windowed_check = $MarginContainer/VBoxContainer/GraphicsSection/VBoxContainer/ModeSection/Windowed
-@onready var windowed_fullscreen_check = $MarginContainer/VBoxContainer/GraphicsSection/VBoxContainer/ModeSection/WindowedFullscreen
-@onready var ambient_slider = $MarginContainer/VBoxContainer/VolumeSection/VBoxContainer3/AmbientVolumeSlider
-@onready var sfx_slider = $MarginContainer/VBoxContainer/VolumeSection/VBoxContainer2/SoundEffectsVolumeSlider
+@onready var return_button =$CenterContainer/PanelContainer/MarginContainer/Layout/ButtonsSection/ReturnButton
+@onready var save_button = $CenterContainer/PanelContainer/MarginContainer/Layout/ButtonsSection/SaveButton
+@onready var resolution_select = $CenterContainer/PanelContainer/MarginContainer/Layout/HBoxContainer/GraphicsSection/GridContainer/SelectResolution
+@onready var mode_select = $CenterContainer/PanelContainer/MarginContainer/Layout/HBoxContainer/GraphicsSection/GridContainer/SelectWindow
+@onready var master_slider = $CenterContainer/PanelContainer/MarginContainer/Layout/HBoxContainer/VolumeSection/VBoxContainer/MasterVolumeSlider
+@onready var sfx_slider = $CenterContainer/PanelContainer/MarginContainer/Layout/HBoxContainer/VolumeSection/VBoxContainer2/SoundEffectsVolumeSlider
+@onready var ambient_slider = $CenterContainer/PanelContainer/MarginContainer/Layout/HBoxContainer/VolumeSection/VBoxContainer3/AmbientVolumeSlider
+
+
 func _ready() -> void:
 	master_slider.value  = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
 	ambient_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Ambient")))
@@ -27,6 +27,21 @@ func _ready() -> void:
 		var res = resolutions[i]
 		resolution_select.set_item_metadata(i, res)
 	resolution_select.item_selected.connect(_on_resolution_selected)
+	
+	mode_select.add_item("Janela")
+	mode_select.set_item_metadata(0, DisplayServer.WINDOW_MODE_WINDOWED)
+	mode_select.add_item("Janela em Tela Cheia")
+	mode_select.set_item_metadata(1, DisplayServer.WINDOW_MODE_FULLSCREEN)
+	mode_select.add_item("Tela Cheia Exclusiva")
+	mode_select.set_item_metadata(2, DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	
+	mode_select.item_selected.connect(_on_mode_selected)
+	
+	var popup_menu = mode_select.get_popup()
+	popup_menu.add_theme_icon_override("checked", Texture2D.new())
+	popup_menu.add_theme_constant_override("h_separation", 0)
+	popup_menu.add_theme_constant_override("item_h_separation", 0)
+	
 	load_settings()
 	
 func _on_save_pressed():
@@ -52,23 +67,17 @@ func load_settings():
 		sfx_slider.value     = config.get_value("audio", "sfx", sfx_slider.value)
 		resolution_select.select(config.get_value("graphics", "resolution", 0))
 		ambient_slider.value = config.get_value("audio", "ambient", ambient_slider.value)
-		var mode = config.get_value("graphics", "mode", 0)
-		if mode == 0:
-			windowed_check.button_pressed = true
-		elif mode == 4:
-			fullscreen_check.button_pressed = true
-		elif mode == 3:
-			windowed_fullscreen_check.button_pressed = true
+		
+		var mode = config.get_value("graphics", "mode", DisplayServer.WINDOW_MODE_WINDOWED)
+		for i in mode_select.item_count:
+			if mode_select.get_item_metadata(i) == mode:
+				mode_select.select(i)
+				break
 		
 func _on_resolution_selected(idx: int):
 	var res = resolution_select.get_item_metadata(idx)
 	DisplayServer.window_set_size(res)
 	
-func _on_windowed_fullscreen_toggled(toggled_on: bool) -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-
-func _on_fullscreen_toggled(toggled_on: bool) -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-
-func _on_windowed_toggled(toggled_on: bool) -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+func _on_mode_selected(idx: int):
+	var mode = mode_select.get_item_metadata(idx)
+	DisplayServer.window_set_mode(mode)
